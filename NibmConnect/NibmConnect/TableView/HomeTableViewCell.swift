680284d7529f5de165e84541fc911eb6bc3d6c45
Bhanuka Isuru <bhanukaisuru96@gmail.com>
2019-05-20 20:06:26 +0530

    student details page created

---
+++ NibmConnect/NibmConnect/TableView/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  NibmConnect
+//
+//  Created by Bhanuka Isuru on 5/19/19.
+//  Copyright © 2019 Bhanuka Isuru. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
