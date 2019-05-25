cbf1face81df819dec563b4b54228cef32308f78
Bhanuka Isuru <bhanukaisuru96@gmail.com>
2019-05-25 20:44:18 +0530

    my profile and forgot password created

---
+++ NibmConnect/NibmConnect/Model/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  NibmConnect
+//
+//  Created by Bhanuka Isuru on 5/24/19.
+//  Copyright © 2019 Bhanuka Isuru. All rights reserved.
+//
+
+import Foundation
---
+++ NibmConnect/NibmConnect/View/UserService.swift
@@ -0,0 +1,9 @@
+//
+//  UserService.swift
+//  NibmConnect
+//
+//  Created by Bhanuka Isuru on 5/24/19.
+//  Copyright © 2019 Bhanuka Isuru. All rights reserved.
+//
+
+import Foundation
